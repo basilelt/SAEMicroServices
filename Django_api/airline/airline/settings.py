6169261b82d38af49325daf241c02ef6cdd5928c
Basile <lethiecb@protonmail.com>
2024-06-12 13:38:05 +0200

    Docker test

--- Django_api/airline/airline/settings.py
+++ Django_api/airline/airline/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api_client',
     'api_staff',
+    'djecrety',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
