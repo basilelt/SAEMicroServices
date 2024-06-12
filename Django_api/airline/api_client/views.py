b757234f63926d46e2038d564b60351ebe68408a
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2024-06-12 14:44:21 +0200

    3.0

--- Django_api/airline/api_client/views.py
+++ Django_api/airline/api_client/views.py
@@ -10,8 +10,12 @@ from rest_framework import generics
 from .models import Client
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
-from .serializers import FlightSerializer
+
 User = get_user_model()
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class UpdateUserView(APIView):
     def patch(self, request, user_id):
