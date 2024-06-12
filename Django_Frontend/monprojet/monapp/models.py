f2cd1a0f5d0a3cdecf0a79cdd30716ad508c7e69
MaziniiX <bluenderbot@gmail.com>
2024-06-12 11:13:12 +0200

    Add Django forms for AppUser, Staff, Booking, and Flight models

--- Django_Frontend/monprojet/monapp/models.py
+++ Django_Frontend/monprojet/monapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 class AppUser(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
