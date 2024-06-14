d0f4bf1489861263462619b9990e21efd220dc5b
MaziniiX <bluenderbot@gmail.com>
2024-06-14 12:57:40 +0200

    updated database in db.dbml, updated CRUD

--- Django_Frontend/monprojet/monapp/models.py
+++ Django_Frontend/monprojet/monapp/models.py
@@ -1,60 +1,49 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Client(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    groups = models.ManyToManyField('Group', through='ClientGroups')
-    permissions = models.ManyToManyField('Permission', through='ClientPermissions')
 
 class Staff(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    is_staff = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     staff_type = models.ForeignKey('StaffType', on_delete=models.CASCADE)
-    groups = models.ManyToManyField('Group', through='StaffGroups')
-    permissions = models.ManyToManyField('Permission', through='StaffPermissions')
 
 class StaffType(models.Model):
     type = models.CharField(max_length=100)
 
 class Group(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
 class ClientGroups(models.Model):
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 class StaffGroups(models.Model):
-    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 class Permission(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
 class ClientPermissions(models.Model):
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    permission = models.ForeignKey('Permission', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
 
 class StaffPermissions(models.Model):
-    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
-    permission = models.ForeignKey('Permission', on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
 
 class Flight(models.Model):
     flight_number = models.CharField(max_length=10, unique=True)
     departure = models.DateTimeField()
     arrival = models.DateTimeField()
     plane = models.ForeignKey('Plane', on_delete=models.CASCADE)
-    track_origin = models.ForeignKey('Track', on_delete=models.CASCADE, related_name='departure_flights')
-    track_destination = models.ForeignKey('Track', on_delete=models.CASCADE, related_name='arrival_flights')
+    track_origin = models.ForeignKey('Track', related_name='departure_flights', on_delete=models.CASCADE)
+    track_destination = models.ForeignKey('Track', related_name='arrival_flights', on_delete=models.CASCADE)
 
 class Plane(models.Model):
     model = models.CharField(max_length=100)
@@ -65,15 +54,15 @@ class Booking(models.Model):
     booking_date = models.DateTimeField(auto_now_add=True)
     price = models.FloatField()
     booking_type = models.ForeignKey('BookingType', on_delete=models.CASCADE)
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    flight = models.ForeignKey('Flight', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
 
 class BookingType(models.Model):
     type = models.CharField(max_length=30)
 
 class Working(models.Model):
-    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
-    flight = models.ForeignKey('Flight', on_delete=models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
 
 class Airport(models.Model):
     name = models.CharField(max_length=100)
@@ -82,4 +71,4 @@ class Airport(models.Model):
 class Track(models.Model):
     track_number = models.CharField(max_length=10)
     length = models.IntegerField()
-    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
--- Django_Frontend/monprojet/monapp/urls.py
+++ Django_Frontend/monprojet/monapp/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('create-client/', views.client_create_view, name='create-client'),
     path('create-staff/', views.staff_create_view, name='create-staff'),
+    path('create-staff-type/', views.create_staff_type, name='create-staff-type'),
+    path('success/', views.success, name='success'),
+    path('view_staff/', views.view_staff, name='view_staff'),
+    path('view_clients/', views.view_clients, name='view_clients'),
+    path('view_staff_types/', views.view_staff_types, name='view_staff_types'),
 ]
