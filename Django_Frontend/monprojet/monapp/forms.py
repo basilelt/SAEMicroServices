9d2918a65457a82834a45101b1bb0cb453b248f7
MaziniiX <bluenderbot@gmail.com>
2024-06-20 08:53:45 +0200

    commit

--- Django_Frontend/monprojet/monapp/forms.py
+++ Django_Frontend/monprojet/monapp/forms.py
@@ -2,24 +2,37 @@
 from django import forms
 from .models import Client, Staff, StaffType
 
-class ClientForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    is_staff = forms.BooleanField(required=False)
-    is_superuser = forms.BooleanField(required=False)
+from django import forms
+from django.contrib.auth.models import User
+from .models import Client, Staff, StaffType
 
-class StaffForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    is_staff = forms.BooleanField(required=False)
-    is_superuser = forms.BooleanField(required=False)
-    staff_type = forms.IntegerField()
-    
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+class StaffForm(forms.ModelForm):
+    class Meta:
+        model = Staff
+        fields = '__all__'
+
 class StaffTypeForm(forms.ModelForm):
     class Meta:
         model = StaffType
-        fields = ['type']
+        fields = '__all__'
+        
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            client = Client(user=user)
+            client.save()
+        return user
