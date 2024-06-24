8b48521404828711d0126897bc5b650a81d8cdc7
evan <evan.vallet@uha.fr>
2024-06-24 09:04:59 +0200

    fini + statut

--- NATS/banque/banque.py
+++ NATS/banque/banque.py
@@ -1,17 +1,34 @@
 import asyncio
 import nats
+import pickle
+import os
 
-# Dictionnaire des clients avec leurs soldes
-clients = {
-    "client1": 153,
-    "client2": 456,
-    "client3": 789,
-    "client4": 1011,
-    "client5": 1213
-}
+data_dir = "/banque/data"
+data_file = os.path.join(data_dir, "clients.pkl")
 
-# Variable d'autorisation globale
+def save_clients():
+    with open(data_file, "wb") as f:
+        pickle.dump(clients, f)
+
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
+
+if os.path.exists(data_file):
+    with open(data_file, "rb") as f:
+        clients = pickle.load(f)
+else:
+    clients = {
+        "client1": 153,
+        "client2": 456,
+        "client3": 789,
+        "client4": 1011,
+        "client5": 1213
+    }
+    save_clients()
+
 autorize = True
+
+
 
 async def handle_list_accounts_request(msg):
     reply = msg.reply
@@ -29,30 +46,27 @@ async def handle_payment_request(msg):
         client = parts[1]
         montant = float(data)
         
-        # Log for debugging
         print(f"Requête de paiement reçue : client={client}, montant={montant}")
 
         if client in clients:
             if clients[client] >= montant:
-                # Envoyer la demande de validation au client spécifique
-
                 autorize = 'true'
-                
                 print(f"Validation de paiement reçue : autorize={autorize}")
 
                 if autorize:
                     print(f"Paiement de {montant} à {client} autorisé.")
                     clients[client] -= montant
-                    response_msg = f"Paiement autorisé. Nouveau solde de {client}: {clients[client]}"
+                    response_msg = f"True,Payer,Paiement autorisé. Nouveau solde de {client}: {clients[client]}"
+                    save_clients()
                 else:
                     print("Paiement refusé.")
-                    response_msg = "Paiement refusé par l'autorisation."
+                    response_msg = "False,Failed,Paiement refusé par l'autorisation."
             else:
                 print("Paiement refusé, solde insuffisant.")
-                response_msg = "Paiement refusé, solde insuffisant."
+                response_msg = "False,Failed,Paiement refusé, solde insuffisant."
         else:
             print("Client inconnu.")
-            response_msg = "Client inconnu."
+            response_msg = "False,Failed,Client inconnu."
 
         await nc.publish(reply, response_msg.encode())
 
@@ -85,6 +99,7 @@ async def handle_create_account_request(msg):
         if name not in clients:
             clients[name] = balance
             response = f"Compte créé pour {name} avec un solde de {balance}."
+            save_clients()
             print(response)
         else:
             response = f"Le compte {name} existe déjà."
@@ -95,24 +110,47 @@ async def handle_create_account_request(msg):
 
     await nc.publish(reply, response.encode())
 
+async def handle_remboursement_request(msg):
+    try:
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        parts = subject.split('.')
+        client = parts[1]
+        montant = float(data)
+        
+        # Log for debugging
+        print(f"Requête de paiement reçue : client={client}, montant={montant}")
+
+        if client in clients:
+            print(f"remboursement de {montant} à {client}.")
+            clients[client] += montant
+            save_clients()
+            response_msg = f"True,Rembourse,Nouveau solde de {client}: {clients[client]}"
+        else:
+            print("Client inconnu.")
+            response_msg = "False,Failed,Client inconnu."
+
+        await nc.publish(reply, response_msg.encode())
+    except Exception as err:
+        print(err)
+
 async def main():
     global nc
     nc = await nats.connect("nats://192.168.164.130:4222")
     try:
-        await nc.subscribe("banque.demande.client.*", cb=handle_payment_request)
         await nc.subscribe("banque.*.compte", cb=handle_account_request)
         await nc.subscribe("banque.creation", cb=handle_create_account_request)
         await nc.subscribe("banque.list_accounts", cb=handle_list_accounts_request)
         await nc.subscribe("banque.validation.*", cb=handle_payment_request)
-        await nc.subscribe("banque.*.payment", cb=handle_payment_request)
-        
+        await nc.subscribe("banque.*.rembousement", cb=handle_remboursement_request)
+
         while True:
             await asyncio.sleep(1)
     except asyncio.CancelledError:
         pass
     finally:
         await nc.close()
-
 
 if __name__ == '__main__':
     asyncio.run(main())
