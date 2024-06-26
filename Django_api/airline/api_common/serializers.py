3485cd71fd0f934ef68592a4eef94695e2bb7be1
Yulei-cn <116038178+Yulei-cn@users.noreply.github.com>
2024-06-26 18:56:46 +0200

    docstring

--- Django_api/airline/api_common/serializers.py
+++ Django_api/airline/api_common/serializers.py
@@ -4,12 +4,25 @@ from django.contrib.auth.models import User
 from .models import Flight, Booking, Airport, Plane, Transaction, CancellationRequest, PaymentGateway, Track
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle User.
+    """
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_superuser', 'first_name', 'last_name']  # #new3
-        extra_kwargs = {'password': {'write_only': True}}  # 密码字段设为只写
+        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_superuser', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        """
+        Crée un nouvel utilisateur avec les données validées.
+        
+        Args:
+            validated_data (dict): Les données validées pour créer l'utilisateur.
+
+        Returns:
+            User: L'utilisateur créé.
+        """
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
@@ -17,47 +30,76 @@ class UserSerializer(serializers.ModelSerializer):
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', '')
         )
-        return user  # #new3
+        return user
 
 class FlightSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Flight.
+    """
+
     class Meta:
         model = Flight
         fields = '__all__'
 
 class BookingSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Booking.
+    """
+
     class Meta:
         model = Booking
         fields = '__all__'
 
 class AirportSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Airport.
+    """
+
     class Meta:
         model = Airport
         fields = '__all__'
 
 class PlaneSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Plane.
+    """
+
     class Meta:
         model = Plane
         fields = '__all__'
 
-# new-2
 class TransactionSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Transaction.
+    """
+
     class Meta:
         model = Transaction
         fields = '__all__'
 
-# new-2
 class CancellationRequestSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle CancellationRequest.
+    """
+
     class Meta:
         model = CancellationRequest
         fields = '__all__'
 
-# new-2
 class PaymentGatewaySerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle PaymentGateway.
+    """
+
     class Meta:
         model = PaymentGateway
         fields = '__all__'
 
 class TrackSerializer(serializers.ModelSerializer):
+    """
+    Sérialiseur pour le modèle Track.
+    """
+
     class Meta:
         model = Track
         fields = '__all__'
